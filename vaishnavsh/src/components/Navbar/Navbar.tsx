d35da6b82ed43046348a0aa431af9a16ffab514e
vaishnav <vaishnav291999@gmail.com>
2022-12-29 23:50:25 +0530

    add custom scroll bar and add nav elements

--- vaishnavsh/src/components/Navbar/Navbar.tsx
+++ vaishnavsh/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import {useState} from 'react'
+import { Link } from "react-router-dom";
 import './Navbar.scss'
 
+import {AiOutlineHome} from 'react-icons/ai'
+import {BiUser} from 'react-icons/bi'
+import {GoLightBulb} from 'react-icons/go'
+import {BiBook} from 'react-icons/bi'
+import {TbMessage} from 'react-icons/tb'
+import {GrDocumentUser} from 'react-icons/gr'
+import {GrGallery} from 'react-icons/gr'
+import {BsBox} from 'react-icons/bs'
+
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='mobile-view'>
+        <Link to="/" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome className='nav-icon'/></Link>
+        <Link to="/project" onClick={() => setActiveNav('#project')} className={activeNav === '#project' ? 'active' : ''}><GoLightBulb className='nav-icon'/></Link>
+        <Link to="/gallery" onClick={() => setActiveNav('#gallery')} className={activeNav === '#gallery' ? 'active' : ''}><BsBox className='nav-icon'/></Link>
+        <Link to="/contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TbMessage className='nav-icon'/></Link>
+        <Link to="/resume" onClick={() => setActiveNav('#resume')} className={activeNav === '#resume' ? 'active' : ''}><GrDocumentUser className='nav-icon'/></Link>
+      </div>
+
+      <div className='desktop-view'>
+        <Link to="/" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>Home</Link>
+        <Link to="/project" onClick={() => setActiveNav('#project')} className={activeNav === '#project' ? 'active' : ''}>Project</Link>
+        <Link to="/gallery" onClick={() => setActiveNav('#gallery')} className={activeNav === '#gallery' ? 'active' : ''}>3D Art</Link>
+        <Link to="/contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>Contact</Link>
+        <Link to="/resume" onClick={() => setActiveNav('#resume')} className={activeNav === '#resume' ? 'active' : ''}>Resume</Link>
+      </div>
+    </nav>
   )
 }
 
