6126153569bda0142812c284f82dc81c1006335d
vaishnav <vaishnav291999@gmail.com>
2023-01-10 01:17:29 +0530

    add layout for the home page

--- vaishnavsh/src/components/Home/Home.tsx
+++ vaishnavsh/src/components/Home/Home.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Home.scss'
+import Table from '../Table/Table'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="container header">
+      <div className="left">
+        <h1 className="">
+            Vaishnav <span className="primary">Sharma</span>
+        </h1>
+
+        <h2 className="">
+            Programmer, Problem-Solver, 3D Artist and a GEEK.
+        </h2>
+        <div className="">
+            Hi, I am Vaishnav Sharma. I like making interesting projects,
+            learning new technologies and solving problems.
+            I have made projects in web-development, image processing,
+            and 3D animation.
+        </div>
+      </div>
+      <div className="right">
+        <Table></Table>
+      </div>
+    </div>
   )
 }
 
---
+++ vaishnavsh/src/components/Table/Table.tsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/Io0sygr3cjr74XOf/scene.splinecode" />
+  );
+}
