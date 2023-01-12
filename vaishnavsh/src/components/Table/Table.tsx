953fcadedf318732ce003f750551802990619423
vaishnav <vaishnav291999@gmail.com>
2023-01-12 23:57:43 +0530

    add loading for loading model

--- vaishnavsh/src/components/Table/Table.tsx
+++ vaishnavsh/src/components/Table/Table.tsx
@@ -1,7 +1,12 @@
-import Spline from '@splinetool/react-spline';
+import React, { Suspense } from 'react';
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/Io0sygr3cjr74XOf/scene.splinecode" />
+    <>
+    <Suspense fallback={<div className='loading'><p>Loading...</p></div>}>
+      <Spline scene="https://prod.spline.design/7MD6gokbWs9a2Zn4/scene.splinecode" />
+    </Suspense>
+    </>
   );
 }
