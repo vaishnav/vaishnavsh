aec96b54f746faf5fe749ca62ea6e447e8170b7b
vaishnav <vaishnav291999@gmail.com>
2023-01-13 21:03:37 +0530

    add about layout, and glow

--- vaishnavsh/src/components/Home/Home.tsx
+++ vaishnavsh/src/components/Home/Home.tsx
@@ -6,19 +6,22 @@ const Home = () => {
   return (
     <div className="container header">
       <div className="left">
-        <h1 className="">
+        <h4>
+          Hi, my name is 
+        </h4>
+        <p className="header-name">
             Vaishnav <span className="primary">Sharma</span>
-        </h1>
+        </p>
 
-        <h2 className="">
-            Programmer, Problem-Solver, 3D Artist and a GEEK.
-        </h2>
-        <div className="">
+        <p className="header-about">
+            Programmer, Problem-Solver, 3D-Artist and a GEEK.
+        </p>
+        {/* <div className="">
             Hi, I am Vaishnav Sharma. I like making interesting projects,
             learning new technologies and solving problems.
             I have made projects in web-development, image processing,
             and 3D animation.
-        </div>
+        </div> */}
       </div>
       <div className="right">
         <Table></Table>
