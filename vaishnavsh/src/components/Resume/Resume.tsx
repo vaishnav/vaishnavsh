4cbb1b34a175434508d781f2caf284402359c703
vaishnav <vaishnav291999@gmail.com>
2023-01-18 01:23:21 +0530

    add option to view resume, and make it responsive

--- vaishnavsh/src/components/Resume/Resume.tsx
+++ vaishnavsh/src/components/Resume/Resume.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Resume.scss'
+import { Document, Page } from 'react-pdf/dist/esm/entry.vite'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+
+const resumeLink = 'https://raw.githubusercontent.com/vaishnav/vaishnavsh/main/vaishnavsh/src/assets/resume.pdf'
+
 
 const Resume = () => {
+  const [width,updateWidth] = useState(window.innerWidth*0.8)
+
+  useEffect(()=>{
+    const handleResize = ()=>{
+      updateWidth(window.innerWidth*0.8)
+    }
+    window.addEventListener('resize', handleResize)
+  })
+
   return (
-    <div>Resume</div>
+    <div className='container'>
+      <Document file={resumeLink} className='resume-pdf'>
+        <Page pageNumber={1} width={width}/>
+      </Document>
+    </div>
   )
 }
 
