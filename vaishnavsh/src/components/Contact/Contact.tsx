7df4823c886b32737a66cd3f5d6b8e464e57346b
vaishnav <vaishnav291999@gmail.com>
2023-01-21 18:03:50 +0530

    add option to send contact mail

--- vaishnavsh/src/components/Contact/Contact.tsx
+++ vaishnavsh/src/components/Contact/Contact.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './Contact.scss'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef<any>(null);
+
+  const sendEmail = (e:React.SyntheticEvent) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_cg4vkyf', 'template_m6kxgu8', form.current, '7OJ48YVY6Esued1vN')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
-    <div>Contact</div>
+    <div>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
   )
 }
 
