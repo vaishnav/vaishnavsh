216d68b4b9a9f1a524f51aa4c799a9753fe3d3eb
vaishnav <vaishnav291999@gmail.com>
2023-01-26 19:23:09 +0530

    add a temporary mid section

--- vaishnavsh/src/components/Home/Home.tsx
+++ vaishnavsh/src/components/Home/Home.tsx
@@ -12,37 +12,44 @@ import { FaArtstation } from 'react-icons/fa'
 
 const Home = () => {
   return (
-    <div className="container header">
-      <div className="left">
-        <h4>
-          Hi, my name is 
-        </h4>
-        <p className="header-name">
-            Vaishnav <span className="primary">Sharma</span>
-        </p>
+    <>
+      <div className="container header">
+        <div className="left">
+          <h4>
+            Hi, my name is 
+          </h4>
+          <p className="header-name">
+              Vaishnav <span className="primary">Sharma</span>
+          </p>
 
-        <p className="header-about">
-            Programmer, Problem-Solver, 3D-Artist and a GEEK.
-        </p>
-        {/* <div className="">
-            Hi, I am Vaishnav Sharma. I like making interesting projects,
-            learning new technologies and solving problems.
-            I have made projects in web-development, image processing,
-            and 3D animation.
-        </div> */}
-        <div className="header-quick-nav">
-          <a className="quick-nav-item" href='mailto:vaishnav291999@gmail.com'><FiMail/></a>
-          <a className="quick-nav-item" href='https://www.linkedin.com/in/vaishnav-sharma-1952681a3/' target="_blank"><FiLinkedin/></a>
-          <a className="quick-nav-item" href='https://github.com/vaishnav' target="_blank"><FiGithub/></a>
-          <a className="quick-nav-item" href='https://ue837ee2a.artstation.com/' target="_blank"><FaArtstation/></a>
-          <a className="quick-nav-item" href='https://www.instagram.com/vaishnav_1024/' target="_blank"><FiInstagram/></a>
+          <p className="header-about">
+              Programmer, Problem-Solver, 3D-Artist and a GEEK.
+          </p>
+          {/* <div className="">
+              Hi, I am Vaishnav Sharma. I like making interesting projects,
+              learning new technologies and solving problems.
+              I have made projects in web-development, image processing,
+              and 3D animation.
+          </div> */}
+          <div className="header-quick-nav">
+            <a className="quick-nav-item" href='mailto:vaishnav291999@gmail.com'><FiMail/></a>
+            <a className="quick-nav-item" href='https://www.linkedin.com/in/vaishnav-sharma-1952681a3/' target="_blank"><FiLinkedin/></a>
+            <a className="quick-nav-item" href='https://github.com/vaishnav' target="_blank"><FiGithub/></a>
+            <a className="quick-nav-item" href='https://ue837ee2a.artstation.com/' target="_blank"><FaArtstation/></a>
+            <a className="quick-nav-item" href='https://www.instagram.com/vaishnav_1024/' target="_blank"><FiInstagram/></a>
+          </div>
+          <Link to="/contact" className='default-button'>Contact</Link>
         </div>
-        <Link to="/contact" className='default-button'>Contact</Link>
+        <div className="right">
+          <Table></Table>
+        </div>
       </div>
-      <div className="right">
-        <Table></Table>
+      <div className="container mid-section">
+        <h1>
+          I am a technology enthusiast and like to work on real world projects. I've built things in the fields of web developemnt and 3D art. 
+        </h1>
       </div>
-    </div>
+    </>
   )
 }
 
