11ae2e915af73e2e624a233cc2c8d07a8565f1c5
vaishnav <vaishnav291999@gmail.com>
2023-02-05 16:08:20 +0530

    loading message dissappears after scene is loaded

--- vaishnavsh/src/components/Table/Table.tsx
+++ vaishnavsh/src/components/Table/Table.tsx
@@ -1,12 +1,17 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
+import './Table.scss'
 const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 export default function App() {
+  const [showLoading, setShowLoading] = useState(true)
   return (
-    <>
-    <Suspense fallback={<div className='loading'>Loading...</div>}>
-      <Spline scene="https://prod.spline.design/7MD6gokbWs9a2Zn4/scene.splinecode" />
-    </Suspense>
-    </>
+    <div className='table'>
+      <div className="loading">{
+        showLoading ? <h3>Loading...<br/>( Hover once loaded )</h3> : null
+      }</div>
+      <Suspense>
+        <Spline scene="https://prod.spline.design/7MD6gokbWs9a2Zn4/scene.splinecode" onLoad={()=>setShowLoading(false)}/>
+      </Suspense>
+    </div>
   );
 }
