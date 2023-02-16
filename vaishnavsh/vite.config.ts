3ac70cd91f88051c55b0467df71351fc0b94792d
vaishnav <vaishnav291999@gmail.com>
2023-02-17 00:39:22 +0530

    add gh-pages package and deploy the application

--- vaishnavsh/vite.config.ts
+++ vaishnavsh/vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/vaishnavsh/',
+    build: {
+      chunkSizeWarningLimit:2000,
+    },
+    server: {
+        host: true
+    }
 })
